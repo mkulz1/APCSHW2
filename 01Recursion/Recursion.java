f0ffb896387edbbe983ba097b7c1b567d104902d
mkulz1 <mariya.kulyk1@gmail.com>
2015-02-08 19:46:54 -0500

    Added fibonacci method (uses tail recursion)

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -14,6 +14,18 @@ public class Recursion{
 	}
     }
 
+    public int fib(int n){
+	if (n < 0) {
+	    throw new IllegalArgumentException();
+	}
+	return fibH(n, 0, 1);
+    }
+    public int fibH(int n, int x, int y){
+	if (n==0) 
+	    return x;
+	return fibH(n - 1, y, x + y);
+    }
+		  
     public static void main(String[]args){
 
 	Recursion r = new Recursion();
@@ -21,6 +33,8 @@ public class Recursion{
 	System.out.println(r.fact(3));
 	System.out.println(r.fact(4));
 	//	System.out.println(r.fact(-4));
+	System.out.println(r.fib(5));
+	System.out.println(r.fib(10));
 
     } 
 
