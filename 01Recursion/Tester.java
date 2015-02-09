b93238ab580a5adc4eeb722d696c555d8758c180
mkulz1 <mariya.kulyk1@gmail.com>
2015-02-08 19:53:29 -0500

    Made a Tester class

---
+++ 01Recursion/Tester.java
@@ -0,0 +1,17 @@
+public class Tester{
+
+ public static void main(String[]args){
+
+	Recursion r = new Recursion();
+
+	System.out.println(r.fact(3));
+	System.out.println(r.fact(4));
+	//	System.out.println(r.fact(-4));
+	System.out.println(r.fib(5));
+	System.out.println(r.fib(10));
+	System.out.println(r.sqrt(100));
+	System.out.println(r.sqrt(25));
+
+    } 
+
+}
