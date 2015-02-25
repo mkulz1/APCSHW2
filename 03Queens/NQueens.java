9df37fca610ded13d8902e211ae57fcab6724c8f
mkulz1 <mariya.kulyk1@gmail.com>
2015-02-25 17:58:41 -0500

    Help method for solve

--- 03Queens/NQueens.java
+++ 03Queens/NQueens.java
@@ -6,7 +6,7 @@ public class NQueens{
     public String name(){
 	return "kulyk.mariya";
     }
-
+    
     //constants for the class
     //terminal specific character to clear screen , or hide/show cursor
     final static String clear =  "\033[2J";
@@ -21,7 +21,7 @@ public class NQueens{
     private String go(int x,int y){
 	return ("\033[" + x + ";" + y + "H");
     }
- 
+    
     public void wait(int millis){
 	try {
 	    Thread.sleep(millis);
@@ -29,10 +29,10 @@ public class NQueens{
 	catch (InterruptedException e) {
 	}
     }
-
+    
     public String toString(){
 	String ans = "\n";
-
+	
 	for(int i = 0; i < board.length; i++){
 	    for(int j = 0; j < board[i].length; j++){
 		ans = ans + board[i][j] + " ";
@@ -45,16 +45,56 @@ public class NQueens{
     public NQueens(int n){
 	// Makes base board and initialize it
 	board = new int[n][n];
-	for(int i = 0; i < size; i++){
-	    for(int j = 0; j < size; j++){
+	for(int i = 0; i < n; i++){
+	    for(int j = 0; j < n; j++){
 		board[i][j] = 0;
 	    }
 	}
     }
 
     public boolean solve(){
-	board[0][0] = 8;
+	// Start from 0,0
+	return solve(0,board.length);
+    }
 
+    public boolean solve(int x){
+	if (x < board.length || x >= 0)
+	    return solve(x,board.length);
+	return false;
+    }
+
+    public boolean solve(int r, int numQ){
+	//System.out.println(this);
+	//wait(20);
+
+	if(numQ == 0)
+	    return true;
+
+	for(int i = 0; i < board.length; i++){
+
+	}
+	return false;
+
+    }
+
+    public void blockSpots(int row, int col){
+	blockSpots(row,col,0,1);
+	blockSpots(row,col,1,0);
+	blockSpots(row,col,1,1);
+	blockSpots(row,col,0,-1);
+	blockSpots(row,col,-1,0);
+	blockSpots(row,col,-1,1);
+	blockSpots(row,col,1,-1);
+	blockSpots(row,col,-1,-1);
+    }
+
+    public void blockSpots(int row, int col, int y, int x){
+
+	for(int i = row;i < board.length && i > 0; i=i+y){
+	    for(int j = col; j < board.length && j > 0;j= j+x){
+		board[i][j] = 3;
+	    }
+	}
     }
 
 }
