26bd97659952e930f376017970345a86120a2e56
mkulz1 <mariya.kulyk1@gmail.com>
2015-03-03 23:31:33 -0500

    Really confused

--- 05MergeSort/Sorts.java
+++ 05MergeSort/Sorts.java
@@ -1,7 +1,13 @@
 public class Sorts{
 
-    public static void mergesort(int[] a, int[] b){
-
+    public static void mergeSort(int[] a, int[] b){
+	merge(a,b,a.length+b.length);
     }
+    public static void merge(int[] a, int[] b, int length){
+	int[] mergedList = new int[length];	
+
+	if(length == 1)
+    }
+
 
 }
