2ae500de5a7bba8a4166e426469649525d481aff
mkulz1 <mariya.kulyk1@gmail.com>
2015-03-11 23:35:26 -0400

    Created constructor & to string

--- 07LinkedLists/MyLinkedList.java
+++ 07LinkedLists/MyLinkedList.java
@@ -2,5 +2,24 @@ import java.util.*;
 
 public class MyLinkedLIst{
 
+    private lNode head;
+    private lNode current;
+    private int size;
 
+    public MyLinkedList(){
+	head = new LNode(0);
+	current = new LNode(0);
+	size = 0;
+    }
+
+    public String toString(){
+	String result = "[";
+	current = head;
+	while(current.getNext() != null){
+	    result += current.toString() + " ";
+	    current = current.getNext();
+	}
+	result += current.toString();
+	return result + "]";
+    }
 }
