7c7f73bb8fc0a8b861f0dbe563cd8ba96722583b
mkulz1 <mariya.kulyk1@gmail.com>
2015-03-30 14:22:11 -0400

    deq addFirst & addLast

--- 10DEQ/MyDeque.java
+++ 10DEQ/MyDeque.java
@@ -19,11 +19,21 @@ public class MyDeque<T>{
     }
 
     public void addFirst(T value){
-
+	head--;
+	if ( head < 0){
+	    head = deq.length-1; // wraps around
+	}
+	deq[head] = value;
+	size++;
     }
 
     public void addLast(T value){
-
+	tail++;
+	if (tail > deq.length - 1){
+	    tail = 0; // wraps around
+	}
+	deq[tail] = value;
+	size++;
     }
 
     public T getFirst(){
