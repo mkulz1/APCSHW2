7e7c91fd55ad76ec224c1a5e13c1e4b6c9d1bab2
mkulz1 <mariya.kulyk1@gmail.com>
2015-03-30 14:15:49 -0400

    mydeq

---
+++ 10DEQ/MyDeque.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class MyDeque<T>{
+
+    private Object[] deq;
+    private int head; 
+    private int tail;
+    private int size;
+
+    public MyDeque(){
+	deq = new Object[10];
+	head = 5;
+	tail = 4;
+	size = 0;
+    }
+
+    public String name(){
+	return "kulyk.mariya";
+    }
+
+    public void addFirst(T value){
+
+    }
+
+    public void addLast(T value){
+
+    }
+
+    public T getFirst(){
+
+    }
+
+    public T getLast(){
+
+    }
+
+    public void grow(){
+
+    }
+
+    public void shrink(){
+
+    }
