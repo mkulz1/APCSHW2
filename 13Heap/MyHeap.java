720ec6391339de3d72bcbb51f85ab15766732f8f
mkulz1 <mariya.kulyk1@gmail.com>
2015-05-12 13:16:40 -0400

    add Works

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -18,11 +18,12 @@ public class MyHeap{
     }
 
     public String toString(){
-	String result = "";
+	String result = "{ ";
 	for(int i = 0; i < heap.length; i++){
-	    result += heap[i];
+	    result += heap[i] + ",";
 	}
-	return result;
+	result = result.substring(0,result.length()-1);
+	return result + " }";
     }
 
     public int remove(){
@@ -34,11 +35,12 @@ public class MyHeap{
 	int index = heap[0];
 	heap[index] = value;
 	pushUp(index);
+	heap[0] += 1;
     }
     
     public void pushUp(int index){
 
-	while (heap[index] > heap[index/2]) {
+	while ( (heap[index] > heap[index/2]) && index/2 != 0 ) {
 
 	    int hold = heap[index/2];
 	    heap[index/2] = heap[index];
@@ -56,6 +58,11 @@ public class MyHeap{
 
 	MyHeap h = new MyHeap();
 	h.add(3);
+	h.add(8);
+	h.add(67);
+	h.add(4);	
+	h.add(30);
+	System.out.println(h);
     }
 
 }
