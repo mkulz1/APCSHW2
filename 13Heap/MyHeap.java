ea4b5a150bb5f8b9bd42209d96ff26ac3975c99f
mkulz1 <mariya.kulyk1@gmail.com>
2015-05-12 13:11:56 -0400

    Created add Method

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -9,10 +9,12 @@ public class MyHeap{
     public MyHeap(){
 	isMaxHeap = true;
 	heap = new int[10];
+	heap[0] = 1;
     }
     public MyHeap(boolean isMax){
 	isMaxHeap = isMax;
 	heap = new int[10];
+	heap[0] = 1;
     }
 
     public String toString(){
@@ -23,10 +25,37 @@ public class MyHeap{
 	return result;
     }
 
+    public int remove(){
+	return heap[1];
+    }
+
+    public void add(int value){
+
+	int index = heap[0];
+	heap[index] = value;
+	pushUp(index);
+    }
+    
+    public void pushUp(int index){
+
+	while (heap[index] > heap[index/2]) {
+
+	    int hold = heap[index/2];
+	    heap[index/2] = heap[index];
+	    heap[index] = hold;
+
+	    index = index/2;	    
+	} 
+    }
+    
+    public int peek(){
+	return heap[1];
+    }
+
     public static void main(String[]args){
 
 	MyHeap h = new MyHeap();
-
+	h.add(3);
     }
 
 }
